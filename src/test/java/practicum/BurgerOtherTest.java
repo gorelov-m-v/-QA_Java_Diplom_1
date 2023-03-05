f00ad748c18cf7db133a738cbfd36b0aa243a7b3
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-03-05 19:12:32 +0300

    Написал тест на получение рецепта

--- src/test/java/practicum/BurgerOtherTest.java
+++ src/test/java/practicum/BurgerOtherTest.java
@@ -2,16 +2,24 @@ package practicum;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerOtherTest {
-
-    Burger burger = new Burger();
+    @Mock
     Ingredient ingredientFirst;
     Ingredient ingredientSecond;
+    @Mock
+    private Bun bun;
 
+    private final Burger burger = new Burger();
 
     @Test
     public void addIngredientTest() {
@@ -41,5 +49,24 @@ public class BurgerOtherTest {
 
         burger.moveIngredient(1,0);
         Assert.assertEquals(ingredientSecond, burger.ingredients.get(0));
+    }
+
+    @Test
+    public void getReceiptTest() {
+            String result = String.format("(==== some bun ====)%n"
+            + "= filling something =%n"
+            + "(==== some bun ====)%n"
+            +"%n"
+            +"Price: 300,000000%n");
+
+        burger.addIngredient(ingredientFirst);
+        burger.setBuns(bun);
+        Mockito.when(bun.getPrice()).thenReturn(100F);
+        Mockito.when(ingredientFirst.getPrice()).thenReturn(100F);
+        Mockito.when(bun.getName()).thenReturn("some bun");
+        Mockito.when(ingredientFirst.getName()).thenReturn("something");
+        Mockito.when(ingredientFirst.getType()).thenReturn(IngredientType.FILLING);
+        Assert.assertEquals(result, burger.getReceipt());
+
     }
 }
